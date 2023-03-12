83a4ecfe5acba0090b7bab102d6929ba6ffdd2c6
tmdgus95 <8571053@naver.com>
2023-03-12 20:04:06 +0900

    프로젝트 초기설정 4
    라우터 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,26 @@ import { ThemeProvider } from "styled-components";
 import theme from "./assets/styles/theme";
 import GlobalStyle from "./assets/styles/globalStyles";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login";
+import Todo from "./components/Todo";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "login",
+                element: <Login />,
+            },
+            {
+                path: "todo",
+                element: <Todo />,
+            },
+        ],
+    },
+]);
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement,
@@ -11,7 +31,7 @@ root.render(
     // <React.StrictMode>
     <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <App />
+        <RouterProvider router={router} />
     </ThemeProvider>,
     // </React.StrictMode>
 );
